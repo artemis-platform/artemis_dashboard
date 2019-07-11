be62814ece1248a1c8188fe8554b8f64a63be2b7
Chris Laskey <contact@chrislaskey.com>
2019-07-11 21:10:50 +0200

    Refactor into ContextCache module

--- apps/artemis/lib/artemis/context.ex
+++ apps/artemis/lib/artemis/context.ex
@@ -5,6 +5,8 @@ defmodule Artemis.Context do
 
   defmacro __using__(_opts) do
     quote do
+      use Artemis.ContextCache
+
       import Artemis.Context
       import Artemis.Repo.Helpers
       import Artemis.Repo.Order
@@ -12,45 +14,7 @@ defmodule Artemis.Context do
 
       require Logger
 
-      alias Artemis.CacheInstance
       alias Artemis.Event
-
-      # Cache
-
-      def cache(), do: call_with_cache([])
-      def cache(arg1), do: call_with_cache([arg1])
-      def cache(arg1, arg2), do: call_with_cache([arg1, arg2])
-      def cache(arg1, arg2, arg3), do: call_with_cache([arg1, arg2, arg3])
-      def cache(arg1, arg2, arg3, arg4), do: call_with_cache([arg1, arg2, arg3, arg4])
-
-      defp call_with_cache(args) do
-        {:ok, _} = create_cache()
-
-        getter = fn ->
-          apply(__MODULE__, :call, args)
-        end
-
-        # TODO: generate cache key
-        key = "key"
-
-        Artemis.CacheInstance.fetch(__MODULE__, key, getter)
-      end
-
-      defp create_cache() do
-        case CacheInstance.exists?(__MODULE__) do
-          true ->
-            {:ok, "Cache already exists"}
-
-          false ->
-            options = [
-              # TODO: define and get reset events from schema
-              cache_reset_events: [],
-              module: __MODULE__
-            ]
-
-            Artemis.CacheSupervisor.start_child(options)
-        end
-      end
     end
   end
 end
