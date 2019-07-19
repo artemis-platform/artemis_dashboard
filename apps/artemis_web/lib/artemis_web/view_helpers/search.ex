12b15dbc5a4b54804ca344dd9b747735db5d6c04
Chris Laskey <contact@chrislaskey.com>
2019-07-19 16:27:16 -0400

    Cloudant Search Limit Notification

--- apps/artemis_web/lib/artemis_web/view_helpers/search.ex
+++ apps/artemis_web/lib/artemis_web/view_helpers/search.ex
@@ -5,9 +5,7 @@ defmodule ArtemisWeb.ViewHelper.Search do
   Generates class for search form
   """
   def search_class(conn) do
-    query = Map.get(conn.query_params, "query")
-
-    case Artemis.Helpers.present?(query) do
+    case search_present?(conn) do
       true -> "ui search active"
       false -> "ui search"
     end
@@ -18,5 +16,25 @@ defmodule ArtemisWeb.ViewHelper.Search do
   """
   def render_search(conn, options \\ []) do
     Phoenix.View.render(ArtemisWeb.LayoutView, "search.html", conn: conn, options: options)
+  end
+
+  @doc """
+  Diplay search limit notification
+  """
+  def render_cloudant_search_limit_notification(conn) do
+    if search_present?(conn) do
+      body = "Cloudant search returns a maximum of 200 results"
+
+      ArtemisWeb.ViewHelper.Notifications.render_notification("info", body: body)
+    end
+  end
+
+  # Helpers
+
+  defp search_present?(conn) do
+    conn
+    |> Map.get(:query_params, %{})
+    |> Map.get("query")
+    |> Artemis.Helpers.present?()
   end
 end
