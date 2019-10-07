675274cde9fec82c98a3aaedfed81c15cceb73a7
Chris Laskey <contact@chrislaskey.com>
2019-10-07 14:49:28 -0400

    Updated Log In Page Style

--- apps/artemis_web/lib/artemis_web/views/auth_view.ex
+++ apps/artemis_web/lib/artemis_web/views/auth_view.ex
@@ -5,19 +5,12 @@ defmodule ArtemisWeb.AuthView do
     state = Map.get(conn.query_params, "redirect")
 
     available_providers = %{
-      "github" => %{
-        title: "Log in with GitHub",
-        color: "blue",
-        link: Routes.auth_path(conn, :request, "github", state: state)
-      },
       "system_user" => %{
         title: "Log in as System User",
-        color: "red",
         link: Routes.auth_path(conn, :callback, "system_user", state: state)
       },
       "w3id" => %{
         title: "Log in with IBM W3ID",
-        color: "blue",
         link: Routes.auth_path(conn, :request, "w3id", state: state)
       }
     }
