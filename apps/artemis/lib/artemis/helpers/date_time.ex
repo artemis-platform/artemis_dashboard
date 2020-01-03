47222369fd192f09ece3676f662b293baa36922f
Chris Laskey <contact@chrislaskey.com>
2020-01-03 15:38:32 -0500

    Move date helpers into separate helper module

--- apps/artemis/lib/artemis/helpers/date_time.ex
+++ apps/artemis/lib/artemis/helpers/date_time.ex
@@ -1,6 +1,48 @@
 defmodule Artemis.Helpers.DateTime do
 
   @doc """
+  Sort an enumerable by Date
+
+  Calling `sort` on Date structs will return unexpected results, since:
+
+  > In Elixir structs are compared by their contents, in alphabetical order of the fields.
+  > In this case the most significant would be the `day`, then `month` and `year`
+
+  From: https://stackoverflow.com/questions/41655852/sorting-list-of-dates-in-elixir/41655967
+  """
+  def sort_by_date(dates) do
+    Enum.sort_by(dates, & &1, &date_sorter/2)
+  end
+
+  defp date_sorter(date_1, date_2) do
+    case Date.compare(date_1, date_2) do
+      r when r == :lt or r == :eq -> true
+      _ -> false
+    end
+  end
+
+  @doc """
+  Sort an enumerable by DateTime
+
+  Calling `sort` on DateTime structs will return unexpected results, since:
+
+  > In Elixir structs are compared by their contents, in alphabetical order of the fields.
+  > In this case the most significant would be the `day`, then `month` and `year`
+
+  From: https://stackoverflow.com/questions/41655852/sorting-list-of-dates-in-elixir/41655967
+  """
+  def sort_by_date_time(date_times) do
+    Enum.sort_by(date_times, & &1, &date_time_sorter/2)
+  end
+
+  defp date_time_sorter(date_time_1, date_time_2) do
+    case DateTime.compare(date_time_1, date_time_2) do
+      r when r == :lt or r == :eq -> true
+      _ -> false
+    end
+  end
+
+  @doc """
   Adds microseconds to an existing DateTime:
 
       #DateTime<2020-01-03 19:25:05Z>
