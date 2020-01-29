8c7cbf7811dc9c02e66ae183d33de2fde4ba33d7
Chris Laskey <contact@chrislaskey.com>
2020-01-29 18:04:04 -0500

    Support Phoenix LiveView Sockets in User Access Helpers

--- apps/artemis_web/lib/artemis_web/user_access.ex
+++ apps/artemis_web/lib/artemis_web/user_access.ex
@@ -8,11 +8,26 @@ defmodule ArtemisWeb.UserAccess do
 
   alias Plug.Conn
 
+  # Has
+
+  def has?(%Phoenix.LiveView.Socket{} = socket, permission),
+    do: Artemis.UserAccess.has?(socket.assigns.user, permission)
+
   def has?(%Conn{} = conn, permission), do: Artemis.UserAccess.has?(current_user(conn), permission)
   def has?(user, permission), do: Artemis.UserAccess.has?(user, permission)
 
+  # Has Any
+
+  def has_any?(%Phoenix.LiveView.Socket{} = socket, permission),
+    do: Artemis.UserAccess.has_any?(socket.assigns.user, permission)
+
   def has_any?(%Conn{} = conn, permission), do: Artemis.UserAccess.has_any?(current_user(conn), permission)
   def has_any?(user, permission), do: Artemis.UserAccess.has_any?(user, permission)
+
+  # Has All
+
+  def has_all?(%Phoenix.LiveView.Socket{} = socket, permission),
+    do: Artemis.UserAccess.has_all?(socket.assigns.user, permission)
 
   def has_all?(%Conn{} = conn, permission), do: Artemis.UserAccess.has_all?(current_user(conn), permission)
   def has_all?(user, permission), do: Artemis.UserAccess.has_all?(user, permission)
