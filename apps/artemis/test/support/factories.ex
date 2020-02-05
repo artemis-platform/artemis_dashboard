87ec38e2a0760246ed944df360c41507c1d17979
Chris Laskey <contact@chrislaskey.com>
2020-02-05 09:18:37 -0500

    Update Test Factory Relationships

--- apps/artemis/test/support/factories.ex
+++ apps/artemis/test/support/factories.ex
@@ -9,7 +9,7 @@ defmodule Artemis.Factories do
       data: %{},
       type: Faker.Internet.slug(),
       uid: sequence(:uid, &"#{Faker.Internet.slug()}-#{&1}"),
-      user: insert(:user)
+      user: build(:user)
     }
   end
 
@@ -30,7 +30,7 @@ defmodule Artemis.Factories do
       body_html: body,
       topic: Faker.Name.name(),
       title: sequence(:title, &"#{Faker.Name.name()}-#{&1}"),
-      user: insert(:user)
+      user: build(:user)
     }
   end
 
@@ -167,9 +167,9 @@ defmodule Artemis.Factories do
 
   def user_role_factory do
     %Artemis.UserRole{
-      created_by: insert(:user),
-      role: insert(:role),
-      user: insert(:user)
+      created_by: build(:user),
+      role: build(:role),
+      user: build(:user)
     }
   end
 
@@ -179,7 +179,7 @@ defmodule Artemis.Factories do
       section: Faker.Name.name(),
       slug: sequence(:slug, &"#{Faker.Internet.slug()}-#{&1}"),
       title: sequence(:title, &"#{Faker.Name.name()}-#{&1}"),
-      user: insert(:user),
+      user: build(:user),
       weight: :rand.uniform(100)
     }
   end
@@ -190,9 +190,9 @@ defmodule Artemis.Factories do
       section: Faker.Name.name(),
       slug: sequence(:slug, &"#{Faker.Internet.slug()}-#{&1}"),
       title: sequence(:title, &"#{Faker.Name.name()}-#{&1}"),
-      user: insert(:user),
+      user: build(:user),
       weight: :rand.uniform(100),
-      wiki_page: insert(:wiki_page)
+      wiki_page: build(:wiki_page)
     }
   end
 
