80d875ffdb9ebbc56d5c4496f83c15e1e92e0148
Chris Laskey <contact@chrislaskey.com>
2020-06-09 15:20:46 -0400

    Remove schema unique_select_for and unique_for functions

--- apps/artemis/lib/artemis/schema_sql.ex
+++ apps/artemis/lib/artemis/schema_sql.ex
@@ -6,26 +6,6 @@ defmodule Artemis.Schema.SQL do
   defmacro __using__(_options) do
     quote do
       alias Artemis.Repo
-
-      def unique_values_for(field) do
-        __MODULE__
-        |> select(^[field])
-        |> distinct(^field)
-        |> order_by(asc: ^field)
-        |> Repo.all()
-        |> Enum.map(&Map.get(&1, field))
-        |> Enum.reject(&is_nil(&1))
-      end
-
-      def unique_select_values_for(label, value \\ :id) do
-        __MODULE__
-        |> select(^[label, value])
-        |> distinct(^label)
-        |> order_by(asc: ^label)
-        |> Repo.all()
-        |> Enum.reject(&is_nil(Map.get(&1, label)))
-        |> Enum.map(&[key: Map.get(&1, label), value: Map.get(&1, value)])
-      end
     end
   end
 end
